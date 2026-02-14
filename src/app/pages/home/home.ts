0c568c80c3f1cff51337fbecb6c68ee53a17918e
Rob <robchilcottdev@outlook.com>
2026-02-14 12:12:29 +0000

    Prettied up the home screen / menu with cards

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from "@angular/router";
+import { Dock } from "../../components/dock/dock";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, Dock],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 
 export class Home {
+  protected router = inject(Router);
+
+  showScriptSettings(){
+    this.router.navigateByUrl('settings');
+  }
 }
